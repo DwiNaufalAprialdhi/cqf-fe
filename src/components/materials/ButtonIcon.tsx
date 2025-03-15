9790c7ba71249192b33470e965a7cb72e4e6e310
DwiNaufalAprialdhi <dwinaufalaprialdhi@gmail.com>
2025-03-15 08:49:10 +0700

    first-commit

--- src/components/materials/ButtonIcon.tsx
+++ src/components/materials/ButtonIcon.tsx
@@ -9,7 +9,7 @@ type Props = {
 export default function ButtonIcon(props: Props) {
       return (
             <>
-                  <button type='button' className={`p-4 rounded-full bg-theme-ascent flex items-center justify-center gap-x-[14px] hover:bg-opacity-70 duration-300 w-full ${props.className}`}>
+                  <button type='button' className={`px-4 py-2 rounded-full bg-theme-ascent flex items-center justify-center gap-x-[14px] hover:bg-opacity-70 duration-300 w-full ${props.className}`}>
                         <h2 className='font-semibold lg:text-base text-sm leading-[20px] text-slate-50'>{props.children}</h2>
                         <div className='w-5 h-5 rounded-full bg-slate-50 flex items-center justify-center'>
                               <Icon name='arrowRight' className='w-3 h-3 text-theme-ascent' />
